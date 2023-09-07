85c4a2a6bc3c219f7ce77c118a0ee849b605596e
Paulem <66211574+Paulem79@users.noreply.github.com>
2023-09-07 21:11:13 +0200

    Initial commit

---
+++ src/main/java/fr/paulem/things/ThingsClient.java
@@ -0,0 +1,10 @@
+package fr.paulem.things;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ThingsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/fr/paulem/things/ThingsDataGenerator.java
@@ -0,0 +1,11 @@
+package fr.paulem.things;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ThingsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
