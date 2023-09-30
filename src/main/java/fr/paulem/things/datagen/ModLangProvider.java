eb8381ba2898276406c2a3706ac675dbd95e1f15
Paulem <66211574+Paulem79@users.noreply.github.com>
2023-10-01 00:39:38 +0200

    Added le langage de la baguette

--- src/main/java/fr/paulem/things/datagen/ModLangProvider.java
+++ src/main/java/fr/paulem/things/datagen/ModLangProvider.java
@@ -1,10 +1,12 @@
 package fr.paulem.things.datagen;
 
 import fr.paulem.things.datagen.langs.EnglishLangProvider;
+import fr.paulem.things.datagen.langs.FrenchLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ModLangProvider {
     public static void provideLangs(FabricDataGenerator.Pack pack){
         pack.addProvider(EnglishLangProvider::new);
+        pack.addProvider(FrenchLangProvider::new);
     }
 }
---
+++ src/main/java/fr/paulem/things/datagen/langs/FrenchLangProvider.java
@@ -0,0 +1,53 @@
+package fr.paulem.things.datagen.langs;
+
+import fr.paulem.things.block.ModBlocks;
+import fr.paulem.things.item.armors.Armors;
+import fr.paulem.things.item.custom.Customs;
+import fr.paulem.things.item.food.Foods;
+import fr.paulem.things.item.fuels.Fuels;
+import fr.paulem.things.item.ores.Ores;
+import fr.paulem.things.item.seeds.Seeds;
+import fr.paulem.things.item.tools.Tools;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+
+public class FrenchLangProvider extends FabricLanguageProvider {
+    public FrenchLangProvider(FabricDataOutput dataOutput) {
+        super(dataOutput, "fr_fr");
+    }
+
+    @Override
+    public void generateTranslations(TranslationBuilder translationBuilder) {
+        translationBuilder.add(Ores.ADAMANTIUM_INGOT, "Lingot d'adamantium");
+        translationBuilder.add(Ores.RAW_ADAMANTIUM, "Adamantium Brut");
+
+        translationBuilder.add(Foods.TOMATO, "Tomate");
+        translationBuilder.add(Seeds.TOMATO_SEEDS, "Graines de tomate");
+
+        translationBuilder.add(Fuels.COAL_BRIQUETTE, "Fragment de charbon");
+
+        translationBuilder.add(Customs.METAL_DETECTOR, "Détecteur de métaux");
+
+        translationBuilder.add(Tools.ADAMANTIUM_PICKAXE, "Pioche en adamantium");
+        translationBuilder.add(Tools.ADAMANTIUM_AXE, "Hache en adamantium");
+        translationBuilder.add(Tools.ADAMANTIUM_SHOVEL, "Pelle en adamantium");
+        translationBuilder.add(Tools.ADAMANTIUM_SWORD, "Épée en adamantium");
+        translationBuilder.add(Tools.ADAMANTIUM_HOE, "Houe en adamantium");
+
+        translationBuilder.add(Armors.ADAMANTIUM_HELMET, "Casque en adamantium");
+        translationBuilder.add(Armors.ADAMANTIUM_CHESTPLATE, "Plastron en adamantium");
+        translationBuilder.add(Armors.ADAMANTIUM_LEGGINGS, "Jambières en adamantium");
+        translationBuilder.add(Armors.ADAMANTIUM_BOOTS, "Bottes en adamantium");
+
+        translationBuilder.add(ModBlocks.ADAMANTIUM_BLOCK, "Bloc d'adamantium");
+        translationBuilder.add(ModBlocks.RAW_ADAMANTIUM_BLOCK, "Block en adamantium brut");
+
+        translationBuilder.add(ModBlocks.ADAMANTIUM_ORE, "Minerai d'adamantium");
+        translationBuilder.add(ModBlocks.DEEPSLATE_ADAMANTIUM_ORE, "Minerai d'adamantium des abîmes");
+
+        translationBuilder.add("itemgroup.things", "Things");
+
+        translationBuilder.add("messages.things.found", "Minerai trouvé en");
+        translationBuilder.add("messages.things.not_found", "Pas de minerai trouvé !");
+    }
+}
